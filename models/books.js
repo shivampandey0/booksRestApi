1bb73e36423d4ece8c2b66b8a4ed7ed28adec2f8
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-09 23:31:16 +0530

    Book Schema

--- models/books.js
+++ models/books.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Author = require("./author");
+const yup = require("yup");
 
 // Book Schema
 const BookSchema = new mongoose.Schema({
@@ -18,4 +19,23 @@ const BookSchema = new mongoose.Schema({
   },
 });
 
-module.exports = new mongoose.model("Book", BookSchema);
+const validateBook = (book) => {
+  const schema = yup.object().shape({
+    bookName: yup.string().required().min(3).max(50),
+    authorName: yup.string().required().min(3).max(40),
+    authorAge: yup.number().min(10, "Age must be greater than 10").max(100),
+    genre: yup.string().required().min(3).max(40),
+  });
+
+  return schema
+    .validate(book)
+    .then((book) => book)
+    .catch((error) => {
+      return {
+        message: error.message,
+      };
+    });
+};
+
+exports.Book = new mongoose.model("Book", BookSchema);
+exports.validateBook = validateBook;
